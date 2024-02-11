ada5ea15dda6b961ac9b4816aad28d12182e91ac
Robert Scerri <robert.scerri2@gmail.com>
2024-02-11 23:35:35 +0100

    changed test app to use new functions

--- src/test.c
+++ src/test.c
@@ -4,54 +4,30 @@
 
 #include "rzcommon.h"
 
-int main(int argc, char* argv[]) {
+int main(void) {
     libusb_context* context = NULL;
     libusb_init_context(&context, NULL, 0);
 
-    libusb_device_handle *mouse = libusb_open_device_with_vid_pid(NULL, RZ_VENDOR_ID, 0x0046);
-    if (!mouse) {
-        fprintf(stderr,"Unable to open device\n");
-        return 1;
-    }
+    struct rz_device mouse = rz_open_device(0x0046);
+    struct rz_device mousemat = rz_open_device(0x0c00);
+    struct rz_device kbd = rz_open_device(0x0221);
 
-    libusb_device_handle *mousemat = libusb_open_device_with_vid_pid(NULL, RZ_VENDOR_ID, 0x0c00);
-    if (!mousemat) {
-        fprintf(stderr,"Unable to open device\n");
-        return 1;
-    }
-
-    libusb_device_handle *kbd = libusb_open_device_with_vid_pid(NULL, RZ_VENDOR_ID, 0x0221);
-    if (!kbd) {
-        fprintf(stderr,"Unable to open device\n");
-        return 1;
-    }
-
-    // Claim the interface before performing any communication
-    if (libusb_claim_interface(kbd, 2) < 0) {
-        fprintf(stderr, "Failed to claim interface\n");
-        libusb_close(kbd);
-        libusb_exit(context);
-        return 1;
-    }
-
-    rz_set_brightness(mouse, 0.3f);
+    rz_set_brightness(&mouse, 0.3f);
     sleep(1);
-    rz_set_brightness(mouse, 1.0f);
+    rz_set_brightness(&mouse, 1.0f);
     sleep(1);
-    rz_set_brightness(mousemat, 0.3f);
+    rz_set_brightness(&mousemat, 0.3f);
     sleep(1);
-    rz_set_brightness(mousemat, 1.0f);
+    rz_set_brightness(&mousemat, 1.0f);
     sleep(1);
-    rz_set_brightness(kbd, 0.3f);
+    rz_set_brightness(&kbd, 0.3f);
     sleep(1);
-    rz_set_brightness(kbd, 1.0f);
+    rz_set_brightness(&kbd, 1.0f);
     sleep(1);
 
-    libusb_release_interface(kbd, 2);
-
-    libusb_close(mouse);
-    libusb_close(mousemat);
-    libusb_close(kbd);
+    rz_close_device(mouse);
+    rz_close_device(mousemat);
+    rz_close_device(kbd);
 
     libusb_exit(context);
 }
