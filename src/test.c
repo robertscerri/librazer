0357457693e90a7712479cf11b70223c65ebe995
Robert Scerri <robert.scerri2@gmail.com>
2024-02-14 21:09:48 +0100

    added inline comments

--- src/test.c
+++ src/test.c
@@ -22,21 +22,9 @@ int main(void) {
     rz_set_brightness(&kbd, 1.0f);
     sleep(1);
 
-    struct rz_rgb red = {1.0f, 0, 0};
-    struct rz_rgb green = {0, 1.0f, 0};
-    struct rz_rgb blue = {0, 0, 1.0f};
-
-    rz_set_effect_static(&mouse, red);
-    rz_set_effect_static(&mousemat, red);
-    rz_set_effect_static(&kbd, red);
-    sleep(4);
-    rz_set_effect_static(&mouse, green);
-    rz_set_effect_static(&mousemat, green);
-    rz_set_effect_static(&kbd, green);
-    sleep(4);
-    rz_set_effect_static(&mouse, blue);
-    rz_set_effect_static(&mousemat, blue);
-    rz_set_effect_static(&kbd, blue);
+    rz_set_effect_spectrum(&mouse);
+    rz_set_effect_spectrum(&mousemat);
+    rz_set_effect_spectrum(&kbd);
 
     rz_close_device(mouse);
     rz_close_device(mousemat);
