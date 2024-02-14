aaecfde03c530b7494133bc3466ebee958a29a43
Robert Scerri <robert.scerri2@gmail.com>
2024-02-14 21:12:04 +0100

    opening device using #define pid rather than hardcoded value

--- src/test.c
+++ src/test.c
@@ -4,14 +4,15 @@
 
 #include "rzcommon.h"
 #include "chromacommon.h"
+#include "rzdevices.h"
 
 int main(void) {
     libusb_context* context = NULL;
     libusb_init_context(&context, NULL, 0);
 
-    struct rz_device mouse = rz_open_device(0x0046);
-    struct rz_device mousemat = rz_open_device(0x0c00);
-    struct rz_device kbd = rz_open_device(0x0221);
+    struct rz_device mouse = rz_open_device(RZ_PID_MAMBA_TE_WIRED);
+    struct rz_device mousemat = rz_open_device(RZ_PID_FIREFLY);
+    struct rz_device kbd = rz_open_device(RZ_PID_BLACKWIDOW_CHROMA_V2);
 
     rz_set_brightness(&mouse, 0.3f);
     rz_set_brightness(&mousemat, 0.3f);
