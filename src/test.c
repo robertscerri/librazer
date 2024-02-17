6f170823e061c14309dca31704be6b06bd8c2f1e
Robert Scerri <robert.scerri2@gmail.com>
2024-02-17 10:38:55 +0100

    changed test to work based off of cmd line arguments

--- src/test.c
+++ src/test.c
@@ -4,32 +4,54 @@
 
 #include "rzcommon.h"
 #include "chromacommon.h"
-#include "rzdevices.h"
 
-int main(void) {
+int main(int argc, char **argv) {
     libusb_context* context = NULL;
     libusb_init_context(&context, NULL, 0);
 
-    struct rz_device mouse = rz_open_device(RZ_PID_MAMBA_TE_WIRED);
-    struct rz_device mousemat = rz_open_device(RZ_PID_FIREFLY);
-    struct rz_device kbd = rz_open_device(RZ_PID_BLACKWIDOW_CHROMA_V2);
+    if (argc < 2) {
+        printf("Please specify a product ID");
+        exit(1);
+    }
 
-    rz_set_brightness(&mouse, 0.3f);
-    rz_set_brightness(&mousemat, 0.3f);
-    rz_set_brightness(&kbd, 0.3f);
+    struct rz_device dev = rz_open_device(strtol(argv[1], NULL, 0));
+
+    rz_set_brightness(&dev, 0.3f);
     sleep(1);
-    rz_set_brightness(&mouse, 1.0f);
-    rz_set_brightness(&mousemat, 1.0f);
-    rz_set_brightness(&kbd, 1.0f);
+    rz_set_brightness(&dev, 0);
+    sleep(1);
+    rz_set_brightness(&dev, 1.0f);
     sleep(1);
 
-    rz_set_effect_spectrum(&mouse);
-    rz_set_effect_spectrum(&mousemat);
-    rz_set_effect_spectrum(&kbd);
+    struct rz_rgb red = {1, 0, 0};
+    struct rz_rgb green = {0, 1, 0};
+    struct rz_rgb blue = {0, 0, 1};
+    struct rz_rgb cyan = {0, 1, 1};
+    struct rz_rgb magenta = {1, 0, 1};
+    struct rz_rgb yellow = {1, 1, 0};
+    struct rz_rgb white = {1, 1, 1};
 
-    rz_close_device(mouse);
-    rz_close_device(mousemat);
-    rz_close_device(kbd);
+    rz_set_effect_static(&dev, red);
+    sleep(4);
+    rz_set_effect_static(&dev, green);
+    sleep(4);
+    rz_set_effect_static(&dev, blue);
+    sleep(4);
+    rz_set_effect_static(&dev, cyan);
+    sleep(4);
+    rz_set_effect_static(&dev, magenta);
+    sleep(4);
+    rz_set_effect_static(&dev, yellow);
+    sleep(4);
+    rz_set_effect_static(&dev, white);
+    sleep(4);
+    rz_set_effect_wave(&dev, RZ_CHROMA_EFFECT_WAVE_DIRECTION_LEFT);
+    sleep(2);
+    rz_set_effect_wave(&dev, RZ_CHROMA_EFFECT_WAVE_DIRECTION_RIGHT);
+    sleep(2);
+    rz_set_effect_spectrum(&dev);
+
+    rz_close_device(dev);
 
     libusb_exit(context);
 }
