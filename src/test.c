e4d844a657bbda3f1e04836288cf59a04733d93e
Robert Scerri <robert.scerri2@gmail.com>
2024-04-05 18:49:17 +0200

    removed test code

--- src/test.c
+++ src/test.c
@@ -25,25 +25,7 @@ int main(int argc, char **argv) {
     const struct rz_rgb white = {0xff, 0xff, 0xff};
     const struct rz_rgb black = {0x00, 0x00, 0x00};
 
-    struct rz_rgb testRowValues[16];
-    for (int i = 0; i < 16; i++) {
-        testRowValues[i] = black;
-    }
-    struct rz_rgb_matrix_row clearRow = {0, 15, testRowValues};
-
-    struct rz_rgb row3Values[5] = {cyan, magenta, yellow, red, green};
-    struct rz_rgb_matrix_row testRow;
-    testRow.start = 5;
-    testRow.end = 9;
-    testRow.rgb_values = row3Values;
-
-    struct rz_rgb_matrix_row rows[1] = {testRow};
-
-    struct rz_rgb_matrix testMatrix;
-    testMatrix.row_count = 1;
-    testMatrix.rows = rows;
-
-    rz_set_effect_custom(&dev, &testMatrix);
+//    rz_set_effect_custom(&dev, &testMatrix);
 
     rz_close_device(dev);
 
