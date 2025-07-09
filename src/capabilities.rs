28f2b6d6576c6e327305fb79a3743a554549b551
Robert Scerri <robert.scerri2@gmail.com>
2025-07-09 22:49:01 +0200

    removed outdated main.rs

---
+++ src/capabilities.rs
@@ -0,0 +1,4 @@
+pub enum Capabilities {
+    Chroma,
+    AdjustableDPI,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod razer_device;
 pub mod razer_product;
 
+mod capabilities;
 mod razer_report;
 mod usb_device;
 mod utils;
